ad73ec3aa102e53ac69f5535eea3498c2797680b
Igor <770190@bk.ru>
2024-10-15 22:00:02 +0700

    Added theme switcher to article page

--- pages/articles/[id].tsx
+++ pages/articles/[id].tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import {getArticles} from "@/src/mock_api/article_api";
 import Image from "next/image";
 import {TArticle} from "@/src/types/article";
+import ThemeSwitcher from "@/src/components/ui/theme-switcher/theme-switcher";
 
 export const getStaticPaths = async () => {
     const articles = await getArticles();
@@ -27,7 +28,7 @@ interface IArticlePageProps {
 
 const ArticlePage = ({article}: IArticlePageProps) => {
     return (
-        <div className="w-full max-w-full h-full flex items-center flex-col">
+        <div className="w-full max-w-full h-full flex items-center flex-col relative">
             <div className="w-2/3 flex-center flex-col text-center mt-8 gap-8">
                 <span className="w-full text-xl font-bold ">{article.title}</span>
                 <Image src={article.image} alt={article.title}
@@ -38,6 +39,9 @@ const ArticlePage = ({article}: IArticlePageProps) => {
                 <div className="w-3/5 mb-16">
                     <span className="">{article.body}</span>
                 </div>
+            </div>
+            <div className="absolute top-8 right-8">
+                <ThemeSwitcher/>
             </div>
         </div>
     );
--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -5,7 +5,7 @@ const Header = () => {
     return (
         <div className="w-full -mx-4 h-full flex-center relative">
             <span className="font-bold text-2xl">Test Blog App</span>
-            <div className="h-full w-24 flex-center absolute right-0 top-0">
+            <div className="h-full w-28 flex-center absolute right-0 top-0">
                 <ThemeSwitcher/>
             </div>
         </div>
